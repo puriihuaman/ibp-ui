11998ea2e3ad70d8d61d7bd52dd58e51f870de22
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2023-10-28 20:57:40 -0500

    feat: added articles in administration

--- src/careers/Administration.tsx
+++ src/careers/Administration.tsx
@@ -1,8 +1,30 @@
+import CareerInformation from "./components/CareerInformation";
+import Form from "./components/Form";
+import GraduateProfile from "./components/GraduateProfile";
+import Requirement from "./components/Requirement";
+import Schedule from "./components/Schedule";
+import Title from "./components/Title";
+
 export default function Administration() {
 	return (
-		<main>
-			<div>
-				<h1>Administración de empresas</h1>
+		<main className="max-w-6xl mx-auto px-4 py-8">
+			<div className="flex flex-col lg:flex-row gap-8">
+				<section className="w-full">
+					<CareerInformation
+						title="Administración de Empresa"
+						description="Docentes con experiencia en emprendimiento y enseñanza en instituciones superiores.Desarrollo de las habilidades blandas para la empleabilidad.Asesoramiento en las ideas de negocios y Ferias de emprendimiento.Malla curricular diseñada para formar emprendedores con valores.Convenios y bolsa de empleo.Organización y difusión de talleres,conferencias y congresos."
+					/>
+
+					<GraduateProfile description="Administrar adecuada y éticamente la información de la empresa para contribuir a la toma de decisiones.Liderar y supervisar grupos de trabajo, círculos de calidad y cadenas de valor. Gestionar los procesos de la empresa mediante herramientas tecnológicas.Implementar planes y estrategias para mejorar la rentabilidad de la empresa.Optimizar los procesos de comunicación de la organización para mejorar la experiencia de los clientes.Desarrollar nuevos modelos de negocio y emprendimiento en cualquier rubro." />
+
+					<Schedule />
+
+					<Requirement />
+
+					<Title />
+				</section>
+
+				<Form />
 			</div>
 		</main>
 	);
